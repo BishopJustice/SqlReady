060535bf9aa526c00ea105005aca36e7f8bb9f66
bishopjustice <luke@lyft.com>
2016-06-15 14:29:07 -0700

    Automatically copies result to clipboard

--- js/breaker.js
+++ js/breaker.js
@@ -1,14 +1,23 @@
-    function sqlify() {
-        var seperator = "\n";
-        var input = document.getElementById('input').value;
-        var splat = input.split(seperator);
-        splat = splat.filter(Boolean);
-        for (each in splat) {
-            splat[each] = " '" + splat[each].trim() + "'";
+        function copyToClipboard(element) {
+            var $temp = $("<input>");
+            $("body").append($temp);
+            $temp.val($(element).text()).select();
+            document.execCommand("copy");
+            $temp.remove();
+        }
+
+        function sqlify() {
+            var seperator = "\n";
+            var input = document.getElementById('input').value;
+            var splat = input.split(seperator);
+            splat = splat.filter(Boolean);
+            for (each in splat) {
+                splat[each] = " '" + splat[each].trim() + "'";
             }
-        console.log(splat)
-        
-        var joined = splat.join(",")
-        console.log(joined)
-        document.getElementById("result").innerHTML = joined
-    }
+            var joined = splat.join(",")
+            document.getElementById("result").innerHTML = joined
+            copyToClipboard("#result")
+        }
+
+
+ 
