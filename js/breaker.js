956d3b9b56fbf15ecb8175784f206c5c39ddfb34
bishopjustice <luke@lyft.com>
2016-06-14 17:15:21 -0700

    First commit

---
+++ js/breaker.js
@@ -0,0 +1,16 @@
+var seperator = document.getElementById('seperator').value
+
+var input = document.getElementById('input').value
+
+var splat = input.split(seperator)
+console.log(splat)
+
+
+// for (each in splat){
+//     splat[each] = "'" + splat[each].trim() + "'"
+// }
+
+// console.log(splat)
+// var joined = splat.join(",")
+// console.log(joined)
+
