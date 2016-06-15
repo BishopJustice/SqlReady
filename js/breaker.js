cae878a89476895b09d65eed63bbdf283733113a
bishopjustice <luke@lyft.com>
2016-06-15 13:27:08 -0700

    Works but you need to type in new line for it to work

--- js/breaker.js
+++ js/breaker.js
@@ -6,11 +6,11 @@ var splat = input.split(seperator)
 console.log(splat)
 
 
-// for (each in splat){
-//     splat[each] = "'" + splat[each].trim() + "'"
-// }
+for (each in splat){
+    splat[each] = "'" + splat[each].trim() + "'"
+}
 
-// console.log(splat)
-// var joined = splat.join(",")
-// console.log(joined)
+console.log(splat)
+var joined = splat.join(",")
+console.log(joined)
 
