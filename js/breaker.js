aaea14535305d290c41396fee02fe05efb384018
bishopjustice <luke@lyft.com>
2016-06-15 13:59:04 -0700

    Works just fine, js in seperate file

--- js/breaker.js
+++ js/breaker.js
@@ -1,16 +1,14 @@
-var seperator = document.getElementById('seperator').value
-
-var input = document.getElementById('input').value
-
-var splat = input.split(seperator)
-console.log(splat)
-
-
-for (each in splat){
-    splat[each] = "'" + splat[each].trim() + "'"
-}
-
-console.log(splat)
-var joined = splat.join(",")
-console.log(joined)
-
+    function sqlify() {
+        var seperator = "\n";
+        var input = document.getElementById('input').value;
+        var splat = input.split(seperator);
+        splat = splat.filter(Boolean);
+        for (each in splat) {
+            splat[each] = " '" + splat[each].trim() + "'";
+            }
+        console.log(splat)
+        
+        var joined = splat.join(",")
+        console.log(joined)
+        document.getElementById("result").innerHTML = joined
+    }
