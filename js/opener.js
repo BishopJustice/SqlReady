56f960e24fca303ef4cd486cdc046487bfba1854
bishopjustice <luke@lyft.com>
2016-07-01 07:22:21 -0700

    Opener changes

--- js/opener.js
+++ js/opener.js
@@ -1,5 +1,5 @@
 // http://stackoverflow.com/questions/7064998/how-to-make-a-link-open-multiple-pages-when-clicked
-// http://www.javascript-coder.com/window-popup/javascript-window-open.phtml
+// http://www.javascript-coder.com/window-popup/javascript-window-open.phtml /
 
 
 function openall(kwargs)
